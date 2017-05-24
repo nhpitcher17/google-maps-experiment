aa0d9af6288b08f9009f7c213d9a6ac412301de7
Tom Dunlap <prof_myu@yahoo.com>
2017-05-24 13:36:10 -0400

    working overlay with correct coordinates about to see if I can combine

--- main.js
+++ main.js
@@ -1,46 +1,181 @@
-
-  function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 11,
-      center: {lat: 40.1, lng: -105.2}  // Australia.
-    });
-
-    var directionsService = new google.maps.DirectionsService;
-    var directionsDisplay = new google.maps.DirectionsRenderer({
-      draggable: true,
-      map: map,
-      panel: document.getElementById('right-panel')
-    });
-
-    directionsDisplay.addListener('directions_changed', function() {
-      computeTotalDistance(directionsDisplay.getDirections());
-    });
-
-    displayRoute('Boulder, CO', 'Boulder Public Library, CO', directionsService,
-        directionsDisplay);
+/*var testEven = new Promise(function(resolve, reject) {
+  if (Date.now() % 2 === 0) {
+    resolve('Even');
+  } else {
+    reject('Odd');
   }
+});
 
-  function displayRoute(origin, destination, service, display) {
-    service.route({
-      origin: origin,
-      destination: destination,
-      waypoints: [{location: 'Galvanize, Boulder'}, {location: 'Whole Foods, Boulder'}],
-      travelMode: 'WALKING'
-    }, function(response, status) {
-      if (status === 'OK') {
-        display.setDirections(response);
-      } else {
-        alert('Could not display directions due to: ' + status);
-      }
-    });
-  }
+testEven.then(function(result) {
+  console.log(result);
+}, function(err) {
+  console.error(err);
+});
+*/
 
-  function computeTotalDistance(result) {
-    var total = 0;
-    var myroute = result.routes[0];
-    for (var i = 0; i < myroute.legs.length; i++) {
-      total += myroute.legs[i].distance.value;
+var overlay;
+
+USGSOverlay.prototype = new google.maps.OverlayView();
+
+
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: {lat: 40.02, lng: -105.27},  // Boulder.
+  });
+
+  var bounds = new google.maps.LatLngBounds(
+      new google.maps.LatLng(39.967819, -105.344032),
+      new google.maps.LatLng(40.079471, -105.170608));
+
+  // The photograph is courtesy of the U.S. Geological Survey.
+  var srcImage = '/Users/user/test/google_maps_experiment/images/crime.png';
+
+/*  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer({
+    draggable: true,
+    map: map,
+    panel: document.getElementById('right-panel')
+  });
+
+  directionsDisplay.addListener('directions_changed', function() {
+    computeTotalDistance(directionsDisplay.getDirections());
+  });
+
+  displayRoute('Boulder, CO', 'Boulder Public Library, CO', directionsService,
+    directionsDisplay);
+*/
+  overlay = new USGSOverlay(bounds, srcImage, map);
+}
+
+
+
+function displayRoute(origin, destination, service, display) {
+  service.route({
+    origin: origin,
+    destination: destination,
+    waypoints: [{location: 'Galvanize, Boulder'}, {location: 'Whole Foods, Boulder'}],
+    travelMode: 'WALKING'
+  }, function(response, status) {
+    if (status === 'OK') {
+      display.setDirections(response);
+    } else {
+      alert('Could not display directions due to: ' + status);
     }
-    total = total / 1000;
-    document.getElementById('total').innerHTML = total + ' km';
+});
+}
+
+function computeTotalDistance(result) {
+  var total = 0;
+  var myroute = result.routes[0];
+  for (var i = 0; i < myroute.legs.length; i++) {
+    total += myroute.legs[i].distance.value;
+}
+  total = total / 1000;
+  document.getElementById('total').innerHTML = total + ' km';
+}
+
+function USGSOverlay(bounds, image, map) {
+
+  // Now initialize all properties.
+  this.bounds_ = bounds;
+  this.image_ = image;
+  this.map_ = map;
+
+  // Define a property to hold the image's div. We'll
+  // actually create this div upon receipt of the onAdd()
+  // method so we'll leave it null for now.
+  this.div_ = null;
+
+  // Explicitly call setMap on this overlay
+  this.setMap(map);
+}
+
+/**
+ * onAdd is called when the map's panes are ready and the overlay has been
+ * added to the map.
+ */
+USGSOverlay.prototype.onAdd = function() {
+
+  var div = document.createElement('div');
+  div.style.border = 'none';
+  div.style.borderWidth = '0px';
+  div.style.position = 'absolute';
+
+  // Create the img element and attach it to the div.
+  var img = document.createElement('img');
+  img.src = this.image_;
+  img.style.width = '100%';
+  img.style.height = '100%';
+  div.appendChild(img);
+
+  this.div_ = div;
+
+  // Add the element to the "overlayImage" pane.
+  var panes = this.getPanes();
+  panes.overlayImage.appendChild(this.div_);
+};
+
+USGSOverlay.prototype.draw = function() {
+
+  // We use the south-west and north-east
+  // coordinates of the overlay to peg it to the correct position and size.
+  // To do this, we need to retrieve the projection from the overlay.
+  var overlayProjection = this.getProjection();
+
+  // Retrieve the south-west and north-east coordinates of this overlay
+  // in LatLngs and convert them to pixel coordinates.
+  // We'll use these coordinates to resize the div.
+  var sw = overlayProjection.fromLatLngToDivPixel(this.bounds_.getSouthWest());
+  var ne = overlayProjection.fromLatLngToDivPixel(this.bounds_.getNorthEast());
+
+  // Resize the image's div to fit the indicated dimensions.
+  var div = this.div_;
+  div.style.left = sw.x + 'px';
+  div.style.top = ne.y + 'px';
+  div.style.width = (ne.x - sw.x) + 'px';
+  div.style.height = (sw.y - ne.y) + 'px';
+};
+
+USGSOverlay.prototype.onRemove = function() {
+  this.div_.parentNode.removeChild(this.div_);
+};
+
+// Set the visibility to 'hidden' or 'visible'.
+USGSOverlay.prototype.hide = function() {
+  if (this.div_) {
+    // The visibility property must be a string enclosed in quotes.
+    this.div_.style.visibility = 'hidden';
   }
+};
+
+USGSOverlay.prototype.show = function() {
+  if (this.div_) {
+    this.div_.style.visibility = 'visible';
+  }
+};
+
+USGSOverlay.prototype.toggle = function() {
+  if (this.div_) {
+    if (this.div_.style.visibility === 'hidden') {
+      this.show();
+    } else {
+      this.hide();
+    }
+  }
+};
+
+// Detach the map from the DOM via toggleDOM().
+// Note that if we later reattach the map, it will be visible again,
+// because the containing <div> is recreated in the overlay's onAdd() method.
+USGSOverlay.prototype.toggleDOM = function() {
+  if (this.getMap()) {
+    // Note: setMap(null) calls OverlayView.onRemove()
+    this.setMap(null);
+  } else {
+    this.setMap(this.map_);
+  }
+};
+
+google.maps.event.addDomListener(window, 'load', initMap);
